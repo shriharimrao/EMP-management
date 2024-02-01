3d20b496cb28333d09a293ed15704b33a131164e
shriharimrao <93112966+shriharimrao@users.noreply.github.com>
2024-02-01 14:33:37 +0530

    create a user defined model
    we created a model to store require data

--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=255)
+    phone=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.first_name+" "+self.last_name
