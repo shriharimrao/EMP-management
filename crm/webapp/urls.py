0c16d772f1ca75477f7b121f00ad229b33ee514a
shriharimrao <93112966+shriharimrao@users.noreply.github.com>
2024-02-01 17:11:09 +0530

    create a register and login view
    create a home and register and login view

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("",views.Home.as_view(),name='home'),
+    path("register/",views.Register.as_view(),name='register'),
    
 ]
--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm,LoginForm,RecordForm,UploadForm,UpdateForm 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Record
+from django.contrib import messages
+from django.views import View
 
-# Create your views here.
+
+# homepage
+class Home(View):
+    template_name='webapp/index.html'
+    def get(self,request,*args,**kwargs):
+        return render(request,self.template_name)
+    
+#register
+class Register(View):
+    template_name='webapp/register.html'
+    def get(self,request,*args,**kwargs):
+        form=RegisterForm()
+        context={'form':form}
+        return render(request,self.template_name,context=context)
+    def post(self,request,*args,**kwargs):
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"account created successfully")
+            return redirect("Login")
+        context={'form':form}
+        return render(request,self.template_name,context=context)
+    
+#login
+class Login(View):
+    template_name='webapp/login.html'
+    def get(self,request,*args,**kwargs):
+        form=LoginForm()
+        context={'form':form}
+        return render(request,self.template_name,context=context)
+    def post(self,request,*args,**kwargs):
+        form=LoginForm(request,data=request.POST)
+        if form.is_valid():
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+                return redirect('dashboard')
+        context={'form':form}
+        return render(request,self.template_name,context=context)
+                
